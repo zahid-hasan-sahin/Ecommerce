5a139b7d4cbaaadfe55bd4a8bcacbfc3cbcabf6f
zahid hasan <zahidhasan6112@gmail.com>
2022-04-28 08:40:22 +0600

    Slider fixed

--- home.php
+++ home.php
@@ -191,12 +191,12 @@
               <div class="item">
                 <?php
                 $query = "SELECT * FROM `tblpromopro` pr , `tblproduct` p , `tblcategory` c
-                WHERE pr.`PROID`=p.`PROID` AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 and p.PROSTATS='Available' limit 3,6";
+                WHERE pr.`PROID`=p.`PROID` AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 and p.PROSTATS='Available' limit 3,3";
 
                 if (isset($_SESSION['CUSID'])) {
                   $userId = $_SESSION['CUSID'];
                   $query = "SELECT * FROM `tblpromopro` pr , `tblproduct` p , `tblcategory` c
-                  WHERE pr.`PROID`=p.`PROID` and p.USERID!='" . $userId . "' AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 and p.PROSTATS='Available' limit 3,6";
+                  WHERE pr.`PROID`=p.`PROID` and p.USERID!='" . $userId . "' AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 and p.PROSTATS='Available' limit 3,3";
                 }
                 $mydb->setQuery($query);
                 $cur = $mydb->loadResultList();
@@ -208,11 +208,11 @@
                     <input type="hidden" id="PROQTY" name="PROQTY" value="<?php echo $result->PROQTY; ?>">
 
                     <input type="hidden" name="PROID" value="<?php echo $result->PROID; ?>">
-                    <div class="col-sm-5">
+                    <div class="col-sm-4">
                       <div class="product-image-wrapper">
                         <div class="single-products">
                           <div class="productinfo text-center">
-                            <img src="<?php echo web_root . 'customer/sell/products/' . $result->IMAGES; ?>" alt="" width="300" height="300" />
+                            <img src="<?php echo web_root . 'customer/sell/products/' . $result->IMAGES; ?>" alt="" />
                             <h2>¥ <?php echo $result->PRODISPRICE; ?></h2>
                             <h5><del>¥ <?php echo $result->ORIGINALPRICE; ?></del></h5>
                             <p><?php echo    $result->PRODESC; ?> || <?php echo    $result->CAMPUS; ?> || <?php echo    $result->BOOKCONDITION; ?> </p>
